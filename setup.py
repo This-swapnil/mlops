026235e11a5384e5bacdb69f75e28ff6c10cdbfe
This-swapnil <sswapnil0098@gmail.com>
2021-09-11 20:00:37 +0530

    pytest example

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(
-    name="src",version="0.0.1",description="its a air tempereture",
-    athor='swapnil sonawane',packages=find_packages(),license="MIT"
-)
+setup(name="src",
+      version="0.0.1",
+      description="its a air tempereture",
+      athor='swapnil sonawane',
+      packages=find_packages(),
+      license="MIT")
