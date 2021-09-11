8c18a5d9f9b4f3dd60affcb9b2aba12a7f4bc6e3
This-swapnil <sswapnil0098@gmail.com>
2021-09-11 12:36:32 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",version="0.0.1",description="its a air tempereture",
+    athor='swapnil sonawane',packages=find_packages(),license="MIT"
+)
